ee73d7a4d00d65f9e875b3058defe536fce9639d
{Himanshu <{himanshu@123gmail.com}>
2026-03-20 23:22:55 +0530

    Initialize project and add description

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🎨 The Art of Finding (project_INT428)
+
+**The Art of Finding** is an interactive web-based exploration tool designed to help users discover and locate public art pieces by world-renowned artists. By searching for an artist's name (e.g., Monet, Rembrandt, or Van Gogh), users can view a collection of their works and see exactly where those pieces are physically located on a map.
+
+### 🌟 Key Features
+- **Artist Discovery:** Search for artists through the **Artsy API** and retrieve their full catalog of public artworks.
+- **Interactive Carousel:** A fluid, card-based interface built with **Materialize CSS** to browse through multiple art pieces easily.
+- **Location Mapping:** Integrated **Google Maps API** (with Geocoding) that automatically pinpoints the gallery or institution where each piece is housed.
+- **Artwork Metadata:** Detailed cards displaying the artwork’s title, category, medium, and year of creation.
+- **Shop Integration:** Quick-access links to find related art prints and books on Amazon.
+
+### 🛠️ Built With
+- **Frontend Framework:** [Materialize CSS](http://materializecss.com/)
+- **Core Logic:** JavaScript & [jQuery](https://jquery.com/)
+- **Data APIs:**
+  - [Artsy API](https://developers.artsy.net/) (Artist and Artwork Data)
+  - [Google Maps Platform](https://developers.google.com/maps) (Interactive Maps and Geocoding)
+
+### 👥 Developed By (Team Nerd)
+- Faiz Hasan
+- Himanshu Shekhar
+- Priyanshu Singh
---
+++ assets/javascript/app.js
@@ -0,0 +1,367 @@
+///////Google Maps API/////////
+'use strict';
+//Google Maps API global variables
+var map;
+var addressLng;
+var addressLat;
+var addressFormatted;
+var LocationString;
+
+
+//Initialize Map
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: { lat: 32.2350428, lng: -110.9547842 }
+  });
+
+  addMarker(32.2350428, -110.9547842, 'U of A');
+  $('#galleryName').text('');
+  $('#galleryAddr').text('');
+  $('#map').attr('title','locationMap');
+}
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 13,
+//     // Updated coordinates for Delhi, India
+//     center: { lat: 28.6139, lng: 77.2090 }
+//   });
+
+//   // Updated marker for Delhi
+//   addMarker(28.6139, 77.2090, 'Delhi');
+//   $('#galleryName').text('');
+//   $('#galleryAddr').text('');
+// }
+
+//Resets the map to the entire United States
+function resetMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: { lat: 37.50, lng: -95.35 }
+  });
+    
+  $('#galleryName').text('');
+  $('#galleryAddr').text('');
+}
+
+//Add marker function for Google maps
+function addMarker(locLat, locLng, locTitle) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locLat, locLng),
+    title: locTitle,
+    map: map
+  });
+}
+
+//Renders the map based off the string of a location
+function displayMap(LocationString) {
+  // Get the latitude and longitude of the Address from the Address String passes in.
+  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + LocationString + '&key=AIzaSyCSCJJqp5sR0Q6TMspPZ_SeqnTC7iLPnmE')
+    .done(function (data) {
+      var address = JSON.stringify(data);
+      addressLng = data.results[0].geometry.location.lng;
+      addressLat = data.results[0].geometry.location.lat;
+      addressFormatted = data.results[0].formatted_address;
+      // Create the map with the lat/lng retrieved from the API.
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: { lat: Number(addressLat), lng: Number(addressLng) },
+      });
+      addMarker(addressLat, addressLng, LocationString);
+      $('#galleryName').text(LocationString);
+      $('#galleryAddr').text(addressFormatted);
+    });
+}
+
+
+$(document).ready(function () {
+
+  //Allows modals to be triggered
+  initializeModal();
+ 
+ 
+ 
+ 
+  $('#map').bind('DOMSubtreeModified', function() {
+    //your code here       
+    $('iframe').attr('title','auto');
+    
+  });
+  ////////ARTSY API//////////
+
+  //Establish global variables for API call token
+
+  var clientID = 'f3a6bc6bafa647a1b773';
+  var clientSecret = '301b70ba99405fc2273322fe97ae23d1';
+  var tokenUrl = 'https://api.artsy.net/api/tokens/xapp_token?';
+  var xappToken;
+
+  //Artsy API Token query
+  function initializeQuery() {
+    $.ajax({
+      url: tokenUrl + 'client_id=' + clientID + '&client_secret=' + clientSecret,
+      method: 'POST'
+    }).done(function (res) {
+      xappToken = res.token;
+    }).fail(function () {
+      console.error('Failed to fetch Xapp-Token. Please check your client ID and secret.');
+      alert('Error: Unable to fetch API token. Please try again later.');
+    });
+  }
+
+  var url = 'https://api.artsy.net/api/';
+
+  var artistId;
+
+  var imageArray = [];
+
+  var artistName;
+
+  //On click listener for arist search
+  $('#submit').on('click', function (event) {
+
+    emptyCard();
+
+    emptyCarousel();
+
+    event.preventDefault();
+
+    imageArray = [];
+
+    getArtistId();
+
+  });
+
+  //Queries the API to find the ID of the artist
+  function getArtistId() {
+
+    artistName = $('#artist-name').val().trim();
+
+    if (!artistName) {
+      alert('Please enter an artist name.');
+      return;
+    }
+
+    var artistNameQuery = url + 'search?q=' + artistName;
+
+    $.ajax({
+      url: artistNameQuery,
+      method: 'GET',
+      beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
+    }).done(function (data) {
+
+      var artistResults = data._embedded.results.filter(function (item) {
+        return item.type === 'artist';
+      });
+
+      if (artistResults.length === 0) {
+        alert('No artist found. Please try a different name.');
+        return;
+      }
+
+      var artistId = artistResults[0]._links.self.href.split('/').pop();
+
+      artistIdQueryFunc(artistId);
+
+      artistName = artistResults[0].title;
+    }).fail(function () {
+      console.error('Failed to fetch artist data. Please check your API token or network connection.');
+      alert('Error: Unable to fetch artist data. Please try again later.');
+    });
+
+  }
+
+  //Queries the API based on the artist ID, returns a list of all public artworks
+  function artistIdQueryFunc(id) {
+    var artistIdQuery = url + 'artworks?artist_id=' + id;
+    $.ajax({
+      url: artistIdQuery,
+      method: 'GET',
+      beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
+    }).done(function (results) {
+
+      if (results._embedded.artworks.length == 0) {
+        noArtFound();
+      }
+
+      for (var i = 0; i < results._embedded.artworks.length; i++) {
+
+        var arrayId = results._embedded.artworks[i].id;
+
+        getArtwork(arrayId, i);
+      }
+
+    });
+  }
+
+  //Takes each artwork id, takes the details of each piece, adds to an object whcih is then pushed to the array
+  function getArtwork(id, counter) {
+    var artworkQuery = url + '/artworks/' + id;
+    $.ajax({
+      url: artworkQuery,
+      method: 'GET',
+      beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
+    }).done(function (results) {
+
+      var image = results._links.thumbnail.href;
+      var collectingInstitution = results.collecting_institution;
+      var id = results.id;
+      var medium = results.medium;
+      var title = results.title;
+      var date = results.date;
+      var category = results.category;
+
+      imageArray.push({
+        arrayId: counter,
+        imgId: id,
+        imgUrl: image,
+        title: title,
+        institution: collectingInstitution,
+        medium: medium,
+        date: date,
+        category: category
+
+      });
+
+      renderCarousel(imageArray);
+
+      renderImage(imageArray[0].imgUrl, imageArray[0].title);
+
+      if (!(imageArray[0].institution)) {
+        resetMap();
+      }
+      else {
+        displayMap(imageArray[0].institution);
+      }
+
+      renderContent(artistName, imageArray[0].title, imageArray[0].category, imageArray[0].date, imageArray[0].medium, imageArray[0].imgUrl);
+
+    });
+
+  }
+
+  //Takes in the array of images and renders the images to the carousel
+  function renderCarousel(array) {
+
+    emptyCarousel();
+
+    $(array).each(function (key, value ) {
+      var imgDiv = $('<img>')
+        .attr('src', value.imgUrl)
+        .attr('id', 'carousel-image')
+        .attr('data', value.arrayId)
+        .attr('alt',value.title)
+        .css('width', '100%')
+        .css('height', '100%')
+        .attr('key',key);
+
+      var aDiv = $('<a>')
+        .addClass('carousel-item')
+        .html(imgDiv);
+
+      $('.carousel').append(aDiv);
+
+    });
+
+    activateCarousel();
+  }
+
+  function activateCarousel() {
+    if ($('.carousel').hasClass('initialized')) {
+      $('.carousel').removeClass('initialized');
+
+    }
+    $('.carousel').carousel({padding: 50}, {shift: 50}, {dist: 0});
+  }
+
+  $('.carousel').on('click', '#carousel-image', function () {
+
+    var imgSrc = $(this).attr('src');
+    var clickId = parseInt($(this).attr('data'));
+
+    var artLocation;
+    var artTitle;
+    var artCategory;
+    var artMedium;
+    var artDate;
+
+    for (var i = 0; i < imageArray.length; i++) {
+
+      if (imageArray[i].arrayId === clickId) {
+        artLocation = imageArray[i].institution;
+        artTitle = imageArray[i].title;
+        artMedium = imageArray[i].medium;
+        artDate = imageArray[i].date;
+        artCategory = imageArray[i].category;
+      }
+    }
+
+    renderImage(imgSrc, artTitle);
+    if (!(artLocation)) {
+      resetMap();
+    }
+    else {
+      displayMap(artLocation);
+    }
+
+    renderContent(artistName, artTitle, artCategory, artDate, artMedium, imgSrc);
+
+  });
+
+  function renderImage(img, title) {
+
+    emptyCard();
+
+    var imgDiv = $('<img>')
+      .attr('alt', title).attr('src', img);
+
+    $('#card-image').html(imgDiv).attr('alt', title);
+
+    $('#art-title').html(title + '<i class="material-icons right">more_vert</i>');
+    $('#link').html('<a href=https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + 'fine%20art%20' + encodeURI(title) + '%20' + encodeURI(artistName) + ' target="_blank">Find on Amazon</a>');
+
+  }
+
+  function renderContent(name, title, category, date, medium, img) {
+    $('.card-reveal').empty();
+
+    var wrapperDiv = $('<div>');
+    var imgDiv = $('<img>').attr('src', img).attr('alt',title+name);
+
+    wrapperDiv.append(imgDiv);
+
+    wrapperDiv.append(
+      '<span class="card-title grey-text text-darken-4">' + title + '<i class="material-icons right">close</i></span>' +
+            '<div>' +
+            '<p>' + 'Category: ' + category + '</p>' +
+            '<p>' + 'Medium: ' + medium + '</p>' +
+            '<p>' + 'Date Created: ' + date + '</p>' +
+            '</div>'
+    );
+
+    $('.card-reveal').append(wrapperDiv);
+  }
+
+  function emptyCard() {
+    $('#card-image').empty();
+  }
+
+  function initializeModal() {
+    $('.modal').modal();
+  }
+
+  function noArtFound() {
+    $('#modal1').modal('open');
+    $('#modal1').css('z-index', 99999);
+  }
+
+  function emptyCarousel() {
+    $('.carousel').empty();
+  }
+
+  initializeQuery();
+
+});
+
+
